76bf43bfa20e0ed268ddeae8fb61516f57feb05e
0kaa <thebestoka0@gmail.com>
2022-01-19 11:42:06 +0200

    📦 NEW: Static Pages Slug

--- app/Http/Resources/Api/StaticPagesResource.php
+++ app/Http/Resources/Api/StaticPagesResource.php
@@ -14,10 +14,19 @@ class StaticPagesResource extends JsonResource
      */
     public function toArray($request)
     {
+        if ($this->slug == 'about-us') {
+            $title = __('local.about_app');
+        } elseif ($this->slug == 'privacy-policy') {
+            $title = __('local.privacy_app');
+        } elseif ($this->slug == 'user-agreement') {
+            $title = __('local.terms_app');
+        } elseif ($this->slug == 'help-center') {
+            $title = __('local.help_app');
+        }
         return [
-            'id' => $this->id,
-            'title'=> $this->title,
-            'desc'=> $this->content,
+            'id'        => $this->id,
+            'title'     => $title,
+            'desc'      => $this->content,
         ];
     }
 }
