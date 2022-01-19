7ace200fc1ebe503a83af2172a98a6c0cf93195a
0kaa <thebestoka0@gmail.com>
2022-01-19 11:42:28 +0200

    📦 NEW: Cart Resource

--- app/Http/Resources/Api/CartResource.php
+++ app/Http/Resources/Api/CartResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources\Api;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ActivityResource extends JsonResource
+class CartResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -14,12 +14,6 @@ class ActivityResource extends JsonResource
      */
     public function toArray($request)
     {
-        return [
-            'id'        => $this->id,
-            'name'      => $this->name,
-            'type'      => $this->type,
-            'image'     => $this->image,
-            'stores'    => StoresResource::collection($this->store),
-        ];
+        return parent::toArray($request);
     }
 }
